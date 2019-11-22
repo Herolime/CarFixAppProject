5bce030c66309e1dc7dfd6c0de2795143e9ac094
Herandy <emilesanen@gmail.com>
2019-11-22 06:18:46 -0400

    Added Action button for navigation purposes

--- MainMenu.js
+++ MainMenu.js
@@ -1,37 +1,82 @@
 import React from 'react';
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
 import styles from './main-styles';
 import {Button} from 'react-native-elements';
 
-const MainMenu: () => React$Element<any> = () => {
-  return (
-    <View style={{height: '100%', width: '100%'}}>
-      <ImageBackground
-        style={{flex: 43}}
-        source={require('./assets/car-repairs-1_640x420.jpg')}>
-        <View style={{marginTop: '3%'}}>
-          <Text style={[styles.fontSizeOne, styles.fontColorPrincipal]}>
-            Mantenimiento
-          </Text>
-          <Text style={[styles.fontSizeOne, styles.fontColorPrincipal]}>
-            Sin Estres.
-          </Text>
+class MainMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handlePress = this.props.onHandlePress
+      ? this.props.onHandlePress
+      : () => {};
+  }
+
+  render() {
+    return (
+      <View style={{height: '100%', width: '100%'}}>
+        <ImageBackground
+          style={{flex: 43}}
+          source={require('./assets/car-repairs-1_640x420.jpg')}>
+          <View style={{marginTop: '3%'}}>
+            <Text style={[styles.fontSizeOne, styles.fontColorPrincipal]}>
+              Mantenimiento
+            </Text>
+            <Text style={[styles.fontSizeOne, styles.fontColorPrincipal]}>
+              Sin Estres.
+            </Text>
+          </View>
+        </ImageBackground>
+        <View style={{flex: 57}}>
+          <Button
+            buttonStyle={[
+              MainMenuStyles.Button,
+              {marginTop: 90, alignSelf: 'center'},
+            ]}
+            title="Revisa el Estatus"
+            titleStyle={[styles.Text, styles.fontSizeTwo]}
+          />
+          <TouchableOpacity
+            onPress={this.handlePress}
+            style={[
+              MainMenuStyles.TouchableOpacity,
+              {marginTop: 40, alignSelf: 'flex-end', marginRight: 18.2},
+            ]}>
+            <Text
+              style={[
+                styles.fontSizeOne,
+                styles.fontColorPrincipal,
+                {top: '21.5%'},
+              ]}>
+              +
+            </Text>
+          </TouchableOpacity>
         </View>
-      </ImageBackground>
-      <View style={{flex: 57, alignItems: 'center'}}>
-        <Button
-          buttonStyle={[MainMenuStyles.Button, {marginTop: 90}]}
-          title="Revisa el Estatus"
-          titleStyle={[styles.Text, styles.fontSizeTwo]}
-        />
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const MainMenuStyles = StyleSheet.create({
   View: {
     flex: 100,
+  },
+  TouchableOpacity: {
+    height: 60,
+    width: 59,
+    borderRadius: 30,
+    backgroundColor: '#FE9E1C',
+    shadowColor: '#000000',
+    shadowOffset: {width: 2, height: 2},
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    alignItems: 'center',
+    textAlign: 'center',
   },
   Button: {
     width: 335,
