d3828441801d0b2a4d51b867cf2360b00f74df2a
Herandy <emilesanen@gmail.com>
2019-12-11 20:41:25 -0400

    Added Component in which user can select (or maybe just display) the shop chose for the service

---
+++ CarDelivery/ConfirmSelectedShop.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text, TextInput} from 'react-native';
+import styles from '../main-styles';
+import CDStyles from './CDStyles';
+
+class ConfirmSelectedShop extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <Text
+          style={[styles.fontColorSecondary, styles.fontSizeTwo, {flex: this.props.titleFlexValue}]}> Taller Seleccionado </Text>
+        <View style={{flex: 8}}>
+          <TextInput
+            style={[
+              styles.TextInput,
+              CDStyles.TextInput,
+              {alignSelf: 'center'},
+            ]}
+            value={this.props.selectedShop}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+export default ConfirmSelectedShop;
