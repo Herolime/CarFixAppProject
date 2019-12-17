452ab50e58625b565647380b83af4fdecb559b24
Herandy <emilesanen@gmail.com>
2019-12-17 09:53:04 -0400

    -Alter navigate method to send, keep and/or alter "State" of navigation. -Added method for accessing navigation params. -Added header to application

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import {Text, Image} from 'react-native';
 import MainMenu from './MainMenu';
 import LocateTrailer from './LocateTrailer';
 import NavigationService from './NavigationService';
@@ -9,70 +10,229 @@ import CarDelivery from './CarDelivery/CDMainScreen';
 import OrderConfirmation from './OrderConfirmation';
 import PaymentMethod from './PaymentMethod';
 import OrderSent from './OrderSent';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class MainScreen extends React.Component {
   constructor(props) {
     super(props);
   }
-
+  static navigationOptions = {
+    headerTitle: () => (
+      <Image
+        style={{width: 65, height: 65}}
+        source={require('./assets/hardware-icon-9.png')}
+      />
+    ),
+    headerRight: () => (
+      <Text style={{marginRight: 5}}>
+        <Icon name="calendar-o" size={30} color={'gray'} />
+      </Text>
+    ),
+  };
   render() {
     return (
-      <MainMenu onHandlePress={() => NavigationService.navigate('Locate')} />
+      <MainMenu
+        onHandlePress={(navProps?) =>
+          NavigationService.navigate(
+            'Locate',
+            !!navProps
+              ? {...navProps}
+              : {
+                  selectedId: 0,
+                },
+          )
+        }
+        navigationProps={property => this.props.navigation.getParam(property)}
+      />
     );
   }
 }
 
 class LocationScreen extends React.Component {
+  static navigationOptions = {
+    headerTitle: () => (
+      <Image
+        style={{width: 65, height: 65}}
+        source={require('./assets/hardware-icon-9.png')}
+      />
+    ),
+    headerRight: () => (
+      <Text style={{marginRight: 5}}>
+        <Icon name="calendar-o" size={30} color={'gray'} />
+      </Text>
+    ),
+  };
   render() {
     return (
       <LocateTrailer
-        onHandlePress={() => NavigationService.navigate('Service')}
+        onHandlePress={(navProps?) =>
+          NavigationService.navigate(
+            'Service',
+            !!navProps
+              ? {...navProps}
+              : {
+                  ...this.props.navigation.state.params,
+                },
+          )
+        }
+        navigationProps={property => this.props.navigation.getParam(property)}
       />
     );
   }
 }
 
 class SelectServiceScreen extends React.Component {
+  static navigationOptions = {
+    headerTitle: () => (
+      <Image
+        style={{width: 65, height: 65}}
+        source={require('./assets/hardware-icon-9.png')}
+      />
+    ),
+    headerRight: () => (
+      <Text style={{marginRight: 5}}>
+        <Icon name="calendar-o" size={30} color={'gray'} />
+      </Text>
+    ),
+  };
   render() {
     return (
       <SelectService
-        navigateTo={() => NavigationService.navigate('Delivery')}
+        navigateTo={(navProps?) =>
+          NavigationService.navigate(
+            'Delivery',
+            !!navProps
+              ? {...navProps, ...this.props.navigation.state.params}
+              : {
+                  ...this.props.navigation.state.params,
+                },
+          )
+        }
       />
     );
   }
 }
 
 class CarDeliveryScreen extends React.Component {
+  static navigationOptions = {
+    headerTitle: () => (
+      <Image
+        style={{width: 65, height: 65}}
+        source={require('./assets/hardware-icon-9.png')}
+      />
+    ),
+    headerRight: () => (
+      <Text style={{marginRight: 5}}>
+        <Icon name="calendar-o" size={30} color={'gray'} />
+      </Text>
+    ),
+  };
   render() {
     return (
       <CarDelivery
-        navigateTo={() => NavigationService.navigate('Confirmation')}
+        navigateTo={(navProps?) =>
+          NavigationService.navigate(
+            'Confirmation',
+            !!navProps
+              ? {...navProps, ...this.props.navigation.state.params}
+              : {
+                  ...this.props.navigation.state.params,
+                },
+          )
+        }
+        navigationProps={property => this.props.navigation.getParam(property)}
       />
     );
   }
 }
 
 class ConfirmationScreen extends React.Component {
+  static navigationOptions = {
+    headerTitle: () => (
+      <Image
+        style={{width: 65, height: 65}}
+        source={require('./assets/hardware-icon-9.png')}
+      />
+    ),
+    headerRight: () => (
+      <Text style={{marginRight: 5}}>
+        <Icon name="calendar-o" size={30} color={'gray'} />
+      </Text>
+    ),
+  };
   render() {
     return (
       <OrderConfirmation
-        navigateTo={() => NavigationService.navigate('Payment')}
+        navigateTo={(navProps?) =>
+          NavigationService.navigate(
+            'Payment',
+            !!navProps
+              ? {...navProps, ...this.props.navigation.state.params}
+              : {
+                  ...this.props.navigation.state.params,
+                },
+          )
+        }
+        navigationProps={property => this.props.navigation.getParam(property)}
       />
     );
   }
 }
 
 class PaymentScreen extends React.Component {
+  static navigationOptions = {
+    headerTitle: () => (
+      <Image
+        style={{width: 65, height: 65}}
+        source={require('./assets/hardware-icon-9.png')}
+      />
+    ),
+    headerRight: () => (
+      <Text style={{marginRight: 5}}>
+        <Icon name="calendar-o" size={30} color={'gray'} />
+      </Text>
+    ),
+  };
   render() {
     return (
-      <PaymentMethod navigateTo={() => NavigationService.navigate('Ordered')} />
+      <PaymentMethod
+        navigateTo={(navProps?) =>
+          NavigationService.navigate(
+            'Ordered',
+            !!navProps
+              ? {...navProps, ...this.props.navigation.state.params}
+              : {
+                  ...this.props.navigation.state.params,
+                },
+          )
+        }
+        navigationProps={property => this.props.navigation.getParam(property)}
+      />
     );
   }
 }
 
 class OrderSentScreen extends React.Component {
+  static navigationOptions = {
+    headerTitle: () => (
+      <Image
+        style={{width: 65, height: 65}}
+        source={require('./assets/hardware-icon-9.png')}
+      />
+    ),
+    headerRight: () => (
+      <Text style={{marginRight: 5}}>
+        <Icon name="calendar-o" size={30} color={'gray'} />
+      </Text>
+    ),
+  };
   render() {
-    return <OrderSent />;
+    return (
+      <OrderSent
+        navigateTo={() => NavigationService.navigate('Main')}
+        navigationProps={property => this.props.navigation.getParam(property)}
+      />
+    );
   }
 }
 
